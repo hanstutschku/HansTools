d8873f31bad42a87c46a9417e52f57f40e418a8b
jsbean <jbean46@gmail.com>
2016-04-22 18:54:05 -0400

    Clean up a little bit

--- SwiftAudioKitTest/AudioPlayerPool.swift
+++ SwiftAudioKitTest/AudioPlayerPool.swift
@@ -9,17 +9,9 @@
 import Foundation
 import AudioKit
 
-class AudioPlayerPool {
-  
+public class AudioPlayerPool {
     
-    //var audioPlayer: AKAudioPlayer! // the exclamation mark makes it an implicit unwrapped optional
-    // if not initialized and called it will crash the app
-    
-    //var audioPlayer2: AKAudioPlayer! // the exclamation mark makes it an implicit unwrapped optional
-    // if not initialized and called it will crash the app
-    
-    //var audioPlayer3: AKAudioPlayer!
-    
+    // Deal with these like the audio players
     var timePitch : AKTimePitch!
     var timePitch2 : AKTimePitch!
     var timePitch3 : AKTimePitch!
@@ -29,66 +21,29 @@ class AudioPlayerPool {
     
     var pitchShift : Double
     
-    init(pitchShift: Double = 0.0){
-        print("initialize AudioPlayerPool")
+    public init(pitchShift: Double = 0.0){
         self.pitchShift = pitchShift
-        
-        // generate audioPlayers
-        
-        
-        //timePitch = makeConfiguredTimePitchNode(audioPlayer: audioPlayer)
-        
         mixer = AKMixer()
         populateAudioPlayers()
         for audioPlayer in audioPlayers { mixer.connect(audioPlayer) }
-        
-        //mixer = AKMixer(audioPlayer)
         AudioKit.output = mixer
         AudioKit.start()
-        //audioPlayer.play()
-        
-    
     }
     
-    private func populateAudioPlayers() {
-        for _ in 0..<3 {
-            let audioPlayer = makeConfiguredAudioPlayer(name: "voice-unrooted-076", volume: 1.0, loops: false)!
-            audioPlayers.append(audioPlayer)
-        }
-    }
-
-    
-    func playFile(name name: String, volume: Double, playerIndex: Int) {
-        print("play \(name)")
-        //AudioKit.stop() // eek
-        
+    public func playFile(name name: String, volume: Double, playerIndex: Int) {
         let audioPlayer: AKAudioPlayer = audioPlayers[playerIndex] // hope its < 3
         audioPlayer.replaceFile(pathForAudioFile(name: name)!)
         stopAudioPlayerIfNecessary(audioPlayer: audioPlayer)
-        //audioPlayer = makeConfiguredAudioPlayer(name: name, volume: volume, loops: false)
-
-        //mixer = AKMixer(audioPlayer)
-        //mixer.connect(audioPlayer)
-        //
-        //AudioKit.output = mixer
-
-        //AudioKit.start()
         audioPlayer.play()
     }
     
-    
-    
-    func stopFile(name name: String){
-        print("stop \(name)")
+    // currently stops ALL audio players
+    public func stopFile(name name: String) {
         audioPlayers.forEach { $0.stop() }
     }
     
     private func makeConfiguredAudioPlayer(name name: String, volume: Double = 1.0, loops: Bool = false) -> AKAudioPlayer? {       // we don't know if it holds a real file path
-        
-        guard let path = pathForAudioFile(name: name) else {
-            return nil                                              // if the path is not valid, we don't create an audio player
-        }
-        
+        guard let path = pathForAudioFile(name: name) else { return nil }
         let audioPlayer = AKAudioPlayer(path) // local audioPlayer
         audioPlayer.volume = volume
         audioPlayer.looping = loops
@@ -110,5 +65,12 @@ class AudioPlayerPool {
     
     private func stopAudioPlayerIfNecessary(audioPlayer audioPlayer: AKAudioPlayer) {
         if !audioPlayer.isStopped { audioPlayer.stop() }
+    }
+    
+    private func populateAudioPlayers() {
+        for _ in 0..<3 {
+            let audioPlayer = makeConfiguredAudioPlayer(name: "voice-unrooted-076", volume: 1.0, loops: false)!
+            audioPlayers.append(audioPlayer)
+        }
     }
 }
